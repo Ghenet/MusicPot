afedc0271070be782701faff8cea4e0970fff4ef
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-03-29 17:17:26 -0700

    adds routes for home and about pages

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask,render_template,url_for,flash, redirect
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '6ca0f4e0b99dcd7bb9ab'
+
+posts =[
+    {
+        'singer': 'Rihanna',
+        'album' : 'Bad Girl',
+        'Genre' : "R&B",
+        'date_released': 'April 20, 2018'
+
+    },
+    {
+        'singer': 'Chris Brown',
+        'album' : 'Royality',
+        'Genre' : "R&B",
+        'date_released': 'May 20, 2015'
+
+    },
+    {
+        'singer': 'Justin Bieber',
+        'album' : 'Baby',
+        'Genre' : "POP",
+        'date_released': 'November, 2010'
+
+    }
+]
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    # return  render_template('home.html', posts_data=posts)
+  return "<h1>This is home</h1>"
+
+
+@app.route('/about')
+def about():
+    # return render_template('about.html',title='Hey About')    
+   return ("<h1>This is About page</h1>")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
