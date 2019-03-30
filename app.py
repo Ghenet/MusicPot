7b10478d8fe0de53c01f73e989f02a7b1a9b2179
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-03-29 18:35:36 -0700

    adds register page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,url_for,flash, redirect
-
+from forms import RegistrationForm
 
 
 app = Flask(__name__)
@@ -40,8 +40,16 @@ def home():
 
 @app.route('/profile')
 def profile():
-     return render_template('profile.html',title='Hey About')    
-  
+     return render_template('profile.html',title='Hey About')  
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))      
+    return render_template('register.html',title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
