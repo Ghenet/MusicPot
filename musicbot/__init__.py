a20e88d67eb11202b028a7803d15a77036eb1642
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-03-30 19:31:02 -0700

    adds validation to username and email

--- musicbot/__init__.py
+++ musicbot/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 
@@ -8,5 +9,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '6ca0f4e0b99dcd7bb9ab'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from musicbot import routes
--- musicbot/forms.py
+++ musicbot/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email,EqualTo
+from wtforms.validators import DataRequired, Length, Email,EqualTo, ValidationError
+from musicbot.models import User
 
 
 #Sign up form
@@ -12,8 +13,21 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+    
+
+   #username validator 
+    def validate_username(self, username):
+        user = User.query.filter_by(username =username.data)
+        if user:
+            raise ValidationError('That username is taken.please choose another one')
 
 
+
+    #email validator
+    def validate_email(self, email):
+        user = User.query.filter_by(email =email.data)
+        if user:
+            raise ValidationError('That email is already taken.Please choose another one')
 #Login Form
 
 class LoginForm(FlaskForm):
