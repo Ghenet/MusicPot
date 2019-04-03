518f74a10a38335b8641c121c067c830d7fd4338
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-04-03 14:06:27 -0700

    adds update inage functionality on account

--- musicbot/forms.py
+++ musicbot/forms.py
@@ -1,33 +1,43 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email,EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from musicbot.models import User
-
+# import musicbot.models
 
 #Sign up form
 
+
+# def validate_username(form, username):
+#     print('username.data')
+#     for user in User.query.filter_by(username=username.data):
+#         if user:
+#             raise ValidationError('Username is taken. Please choose another one')
+
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',validators= [DataRequired(), 
-    Length(min=2,max=20)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
     email = StringField('Email', validators=[DataRequired(),Email()])
     password = PasswordField('Password',validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
     
 
-   #username validator 
+#    username validator
     def validate_username(self, username):
-        user = User.query.filter_by(username =username.data)
-        if user:
-            raise ValidationError('That username is taken.please choose another one')
+        for user in User.query.filter_by(username=username.data):
+            if user:
+                raise ValidationError('Username is taken. Please choose another one')
 
 
 
     #email validator
     def validate_email(self, email):
-        user = User.query.filter_by(email =email.data)
-        if user:
-            raise ValidationError('That email is already taken.Please choose another one')
+        for user in User.query.filter_by(email=email.data):
+            if user:
+                raise ValidationError('Email is taken. Please choose another one')
+
 #Login Form
 
 class LoginForm(FlaskForm):
@@ -35,3 +45,30 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit =SubmitField('Login')
+
+
+#Update account form
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture' ,validators=[FileAllowed(['jpg','png', 'jpeg'])])
+    submit = SubmitField('Update')
+    
+  # username validator 
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            for user in User.query.filter_by(username=username.data):
+                if user:
+                    raise ValidationError('Username is taken. Please choose another one')
+
+    # #email validator
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            for user in User.query.filter_by(email=email.data):
+                if user:
+                    raise ValidationError('Email is taken. Please choose another one')
+
+                
+
+       
--- musicbot/routes.py
+++ musicbot/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template,url_for,flash, redirect, request
 from musicbot import app , db, bcrypt
-from musicbot.forms import RegistrationForm ,LoginForm
+from musicbot.forms import RegistrationForm ,LoginForm , UpdateAccountForm
 from musicbot.models import  User , Post
 from flask_login import login_user , current_user,logout_user, login_required
 
@@ -44,10 +47,14 @@ def profile():
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
+    print('in register')
+    # if current_user.is_authenticated:
+    #     print('in if')
+    #     return redirect(url_for('home'))
     form = RegistrationForm()
+    print(request.form)
     if form.validate_on_submit():
+        print('in other if')
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
@@ -77,10 +84,38 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
+    #resize the image before saving it 
+    
+    
+    form_picture.save(picture_path)
+
+    return  picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html' , title='Account')    
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email    
+    image_file = url_for('static', filename='images/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)    
+
+
