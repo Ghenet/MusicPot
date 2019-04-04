6d3702616c8fffa36a57064c07cd2595d767a601
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-04-03 21:13:36 -0700

    adds the create-post form

--- musicbot/forms.py
+++ musicbot/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from musicbot.models import User
 # import musicbot.models
@@ -69,6 +69,8 @@ class UpdateAccountForm(FlaskForm):
                 if user:
                     raise ValidationError('Email is taken. Please choose another one')
 
-                
-
-       
+# create post form
+class PostForm(FlaskForm):
+    title = StringField('Title',validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')   
