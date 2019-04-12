df1af075b01a10f2de925e059113d9c355e6383e
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-04-11 18:22:01 -0700

    adds configuration to use gmail

--- musicbot/__init__.py
+++ musicbot/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 
@@ -14,5 +16,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'musicpot111@gmail.com'
+app.config['MAIL_PASSWORD'] = 'sanfran2019'
+mail = Mail(app)
 
 from musicbot import routes
